f2cb36c3e1c89f388f206bdd7ab7db52a26fa411
OpThelf <kevin2002huang@hotmail.com>
2023-04-11 23:13:42 +0200

    regarder update

--- main.c
+++ main.c
@@ -16,68 +16,6 @@
 int main(int argc, char ** argv){
 	//Tout les anciens tests sont dans main.txt bien triés pour faire de la place ici
 
-	kvp * key = createKeyVal("key","value");
-	char * ch = kvts(key);
-	//printf("%s\n",ch);
-	kvp * key2 = stkv(ch);
-	char * ch2 = kvts(key2);
-	//printf("%s\n",ch2);
-	freeKeyVal(key2);
-	freeKeyVal(key);
-	free(ch);
-	free(ch2);
-	Commit * c = initCommit();
-	
-	commitSet(c,"key1","value1");
-	commitSet(c,"key2","value2");
-	
-<<<<<<< HEAD
-	Commit * c2 = createCommit("hash");
-	char * val = commitGet(c,"key2");
-	char * val2 = commitGet(c,"key1");
-	//printf("%s\n",val);
-	//printf("%s\n",val2);
-	commitSet(c2,"key3","value3");
-	commitSet(c2,"key4","value4");
-	char * comtostr= cts(c2);
-	//printf("%s\n",comtostr);
-	Commit * c3 = stc(comtostr);
-	char * ctsc3 = cts(c3);
-	//printf("%s\n",ctsc3);
-	ctf(c3,"testex6.txt");
-	Commit * c4 =  ftc("testex6.txt");
-	char *ctsc4 = cts(c4);
-	printf("%s\n",ctsc4);
-	char * hashc4 = blobCommit(c4);
-	freeCommit(c4);
-	free(ctsc4);
-	free(comtostr);
-	freeCommit(c2);
-	freeCommit(c);
-	free(ctsc3);
-	freeCommit(c3);
-	free(hashc4);
-=======
-	//char * save= saveWorkTree(WT,".");
-	
-	//free(path);
-	//free(blob);
-	//free(hash);
-	//free(htf);
-	//freeWorkTree(WT);
-	//free(save);
-	//free(h1);
-	//free(h2);
-	/*int gcm = getChmod("exo5.txt");
-	printf("getChmod(\"exo5.txt\") = %d\n",gcm);*/
-	WorkTree * WT = initWorkTree();
-	appendWorkTree(WT,"name.txt",NULL,777);
-	char * chaineWT = wtts(WT);
-	printf("wt :\n%s\n",chaineWT);
-
-	freeWorkTree(WT);
-	free(chaineWT);
->>>>>>> b4c853ba5e887996abcceeb5b742950578b2df07
 	return 0;
 }
 
