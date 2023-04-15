48e1037fd4fa442bd0bfb5e0eb2b3704c00f53dd
OpThelf <kevin2002huang@hotmail.com>
2023-04-15 14:42:50 +0200

    Test

--- main.c
+++ main.c
@@ -19,12 +19,55 @@
 
 int main(int argc, char ** argv){
 	//Tout les anciens tests sont dans main.txt bien triés pour faire de la place ici
+	system("./myGit init");
+	system("./myGit add f1.txt");
+	system("./myGit add f3.txt");
+	system("./myGit commit master -m \"commit1 sur master\"");
+	system("./myGit branch Branche");
+
+	system("./myGit checkout-branch Branche");
+	system("echo \"Branche 1\" > f1.txt");
+	system("echo \"Branche 3\" > f3.txt");
+	system("./myGit add f1.txt");
+	system("./myGit add f2.txt");
+	system("./myGit add f3.txt");
+	system("./myGit commit Branche -m \"commit1 sur Branche\"");
+	
+	/*
+	system("echo \"master 1\" > f1.txt");
+	system("echo \"master 3\" > f3.txt");
+	system("./myGit add f3.txt");
+	system("./myGit add f1.txt");
+	system("./myGit commit Branche -m \"commit2 sur Branche\"");
+	system("./myGit checkout-branch master");*/
+	
+	
+	WorkTree * bTW= branchToWorkTree("master");
+	//afficheWT(bTW);
+	freeWorkTree(bTW);
+
+	List * Lc= merge("Branche","BRANCHEFUSION");
+	if (Lc!=NULL){
+		char * c = ltos(Lc);
+		printf("conflicts : %s\n",c);
+		createDeletionCommit("Branche",Lc,"commit de suppression des conflits");
+		List * Lc2= merge("Branche","BRANCHEFUSION");
+		free(c);
+		FreeList(Lc);
+		if (Lc2!=NULL){
+			FreeList(Lc2);
+			printf("erreur : conflits non résolus\n");
+			exit(EXIT_FAILURE);
+		}
+
+	}
+	
 	return 0;
 }
 
 
 /*
-Update Simon : 0 leak error jusqu'à merge inclus 
+Update Simon : 0 leak error jusqu'à createDeletionCommit inclus 
 */
 
 
